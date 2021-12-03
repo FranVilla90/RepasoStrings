14f4ae95b4de7739b0dd7fbd288c2a3af8b894cd
Fran Villa <80397839+FranVilla90@users.noreply.github.com>
2021-12-03 18:07:18 +0100

    Ejercicio02 empezado

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/35/3034680e5b54001c1517c742b5f613a2
@@ -0,0 +1,10 @@
+package repasoStrings;
+
+public class Ejercicio02RepasoCadenas {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- RepasoStrings/src/repasoStrings/Ejercicio02RepasoCadenas.java
+++ RepasoStrings/src/repasoStrings/Ejercicio02RepasoCadenas.java
@@ -1,6 +1,17 @@
 package repasoStrings;
+import java.util.Scanner;
+
+/**
+ * Realizar un método al que se le pase una cadena con un número de teléfono (999999999) 
+ * y devuelva una nueva cadena con el teléfono formateado de esta forma 999-99-99-99
+ * 
+ * @author Fran Villa
+ *
+ */
 
 public class Ejercicio02RepasoCadenas {
+	
+	private static Scanner teclado = new Scanner (System.in);
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
