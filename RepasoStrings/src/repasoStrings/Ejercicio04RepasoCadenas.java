20d9d72595b34b5c610cf78ce50016fd8301c1de
Fran Villa <80397839+FranVilla90@users.noreply.github.com>
2021-12-08 13:14:10 +0100

    Ejercicio04

--- RepasoStrings/src/repasoStrings/Ejercicio04RepasoCadenas.java
+++ RepasoStrings/src/repasoStrings/Ejercicio04RepasoCadenas.java
@@ -17,7 +17,8 @@ public class Ejercicio04RepasoCadenas {
 	private static Scanner teclado = new Scanner (System.in);
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Inicio
+		
 
 	}
 
