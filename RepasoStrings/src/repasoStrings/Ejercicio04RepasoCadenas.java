4715721e3b0f2c2daec627607c30137d8184adef
Fran Villa <80397839+FranVilla90@users.noreply.github.com>
2021-12-08 20:51:06 +0100

    Ejercicios terminados

--- RepasoStrings/src/repasoStrings/Ejercicio04RepasoCadenas.java
+++ RepasoStrings/src/repasoStrings/Ejercicio04RepasoCadenas.java
@@ -1,25 +1,70 @@
 package repasoStrings;
+
 import java.util.Scanner;
 
 /**
- * Realizar un método que permita saber si un texto es o no “Guay del Paraguay”. 
- * Se considera que una cadena es “Guay del Paraguay” si las 4 primeras letras y 
- * las 4 últimas son iguales entre sí. 
- * El número 4 debe crearse como constante de manera que el programa siga funcionando si se cambia 
- * el valor de dicha constante.
+ * Realizar un método que permita saber si un texto es o no “Guay del
+ * Paraguay”. Se considera que una cadena es “Guay del Paraguay” si las 4
+ * primeras letras y las 4 últimas son iguales entre sí. El número 4 debe
+ * crearse como constante de manera que el programa siga funcionando si se
+ * cambia el valor de dicha constante.
  * 
  * @author Fran Villa
  *
  */
 
 public class Ejercicio04RepasoCadenas {
-	
-	private static Scanner teclado = new Scanner (System.in);
+
+	private static Scanner teclado = new Scanner(System.in);
+
+	private static final int NUMERO_LETRAS_COINCIDE = 4;
 
 	public static void main(String[] args) {
 		// Inicio
-		
+		String nombreCadena;
+		System.out.println("Introduce una cadena: ");
+		nombreCadena = teclado.nextLine();
 
+		if (esCadenaGuay(nombreCadena) == true) {
+			
+			System.out.println("La cadena es Guay del Paraguay");
+
+		} else {
+			System.out.println("La cadena no es Guay del Paraguay");
+		}
+
+	}
+	
+	//Metodos
+	
+	/**
+	 * Metodo para comprobar si una cadena empieza y termina con las mismas letras
+	 * @param cadena
+	 * @return
+	 */
+
+	public static boolean esCadenaGuay(String cadena) {
+		boolean esCadenaGuay = true;
+		
+		if (cadena.length() >= NUMERO_LETRAS_COINCIDE * 2) {
+			StringBuilder cadenaInicioSB = new StringBuilder();
+			StringBuilder cadenaFinalSB = new StringBuilder();
+			String cadenaInicio, cadenaFinal;
+
+			cadenaInicioSB.append(cadena.substring(0, NUMERO_LETRAS_COINCIDE));
+			cadenaFinalSB.append(cadena.substring(cadena.length() - NUMERO_LETRAS_COINCIDE, cadena.length()));
+
+			cadenaInicio = cadenaInicioSB.toString();
+			cadenaFinal = cadenaFinalSB.toString();
+
+			if (!(cadenaInicio.equalsIgnoreCase(cadenaFinal))) {
+				esCadenaGuay = false;
+			}
+		} else {
+			esCadenaGuay = false;
+		}
+
+		return esCadenaGuay;
 	}
 
 }
--- RepasoStrings/src/repasoStrings/Ejercicio05RepasoCadenas.java
+++ RepasoStrings/src/repasoStrings/Ejercicio05RepasoCadenas.java
@@ -11,7 +11,8 @@ import java.util.Scanner;
  * 
  * NOTAS:
  *    El diminutivo se devolverá siempre en mayúsculas.
- *    Si el dato del sexo es incorrecto se volverá a solicitar. • Debe usarse un método para crear el diminutivo.
+ *    Si el dato del sexo es incorrecto se volverá a solicitar. 
+ *    Debe usarse un método para crear el diminutivo.
  * 
  * @author Fran Villa 
  *
@@ -23,7 +24,118 @@ public class Ejercicio05RepasoCadenas {
 
 	public static void main(String[] args) {
 		// Inicio
+		String palabra, diminutivoPalabra;
+		System.out.println("Introduce una palabra: ");
+		palabra = teclado.nextLine();
+		
+		diminutivoPalabra = creacionDiminutivo(palabra);
+		
+		System.out.println("Tu palabra es: " + palabra + " y el diminutivo es: " + diminutivoPalabra);
 
 	}
+	
+	//Metodos 
+	
+	/**
+	 * Metodo para crear un diminutivo desde una cadena dada
+	 * @param cadena
+	 * @return
+	 */
+	
+	public static String creacionDiminutivo (String cadena) {
+		String creacionDiminutivo = "";
+		char genero;
 
+		
+		int posicionFinal = cadena.length()-1;
+		StringBuilder cadenaSB;
+		cadenaSB = new StringBuilder(cadena);
+		
+		
+		if (cadena.charAt(posicionFinal) == 'a') {
+			creacionDiminutivo = diminutivoFemenino(cadenaSB);
+		} else if (cadena.charAt(posicionFinal) == 'o') {
+			creacionDiminutivo = diminutivoMasculino(cadenaSB);
+			
+		} else if (cadena.charAt(posicionFinal) == 'e' || cadena.charAt(posicionFinal) == 'i' || cadena.charAt(posicionFinal) == 'u' ) {
+			genero = tecladoGenero();
+			if (genero == 'M') {
+				creacionDiminutivo = diminutivoMasculino(cadenaSB);
+			} else {
+				creacionDiminutivo = diminutivoFemenino(cadenaSB);
+			}
+			
+		} else {
+			genero = tecladoGenero();
+			if (genero == 'M') {
+				creacionDiminutivo = diminutivoMasculino(cadenaSB);
+			} else {
+				creacionDiminutivo = diminutivoFemenino(cadenaSB);	
+			}
+		}
+		
+		return creacionDiminutivo; 
+	}
+	/**
+	 * Metodo para crear el diminutivo masculino desde un StringBuilder
+	 * @param cadenaSB
+	 * @return
+	 */
+	
+	public static String diminutivoMasculino (StringBuilder cadenaSB){
+		String diminutivoMasculino;
+		String cadenaDiminutivoMasculino = "ITO";
+		int posicionFinal = cadenaSB.length()-1;
+		
+		
+		if (cadenaSB.lastIndexOf("a") == 0 || cadenaSB.lastIndexOf("e") == 0 || cadenaSB.lastIndexOf("i") == 0 || cadenaSB.lastIndexOf("o") == 0 || cadenaSB.lastIndexOf("u") == 0) {
+			cadenaSB.deleteCharAt(posicionFinal).append(cadenaDiminutivoMasculino);
+			diminutivoMasculino = cadenaSB.toString();
+		} else {
+		
+		cadenaSB.append(cadenaDiminutivoMasculino);
+		diminutivoMasculino = cadenaSB.toString();
+		
+		}
+		
+		return diminutivoMasculino;
+	}
+	
+	/**
+	 * 
+	 * Metodo para crear el diminutivo femenino desde un StringBuilder
+	 * @param cadenaSB
+	 * @return
+	 */
+	
+	public static String diminutivoFemenino (StringBuilder cadenaSB) {
+		String diminutivoFemenino;
+		String cadenaDiminutivoFemenino = "ITA";
+		int posicionFinal = cadenaSB.length()-1;
+		
+		if (cadenaSB.lastIndexOf("a") == 0 || cadenaSB.lastIndexOf("e") == 0 || cadenaSB.lastIndexOf("i") == 0 || cadenaSB.lastIndexOf("o") == 0 || cadenaSB.lastIndexOf("u") == 0) {
+			cadenaSB.deleteCharAt(posicionFinal).append(cadenaDiminutivoFemenino);
+			diminutivoFemenino = cadenaSB.toString();
+		} else {
+			cadenaSB.append(cadenaDiminutivoFemenino);
+			diminutivoFemenino = cadenaSB.toString();
+		}
+		
+		return diminutivoFemenino;
+	}
+	
+	/**
+	 * Metodo para recoger el caracter de Masculino o Femenino
+	 * @return
+	 */
+	
+	public static char tecladoGenero() {
+		char tecladoGenero;	
+		do {
+			System.out.println("Introduce un genero: (M-F)");
+			tecladoGenero = teclado.nextLine().charAt(0);
+		} while (!(tecladoGenero == 'M' || tecladoGenero == 'F' ));
+		
+		return tecladoGenero;
+	}
 }
